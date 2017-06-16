a8b804118875c40812022a1d637bdac4e4e23589
James Higgins <james.milan.higgins@gmail.com>
2017-06-16 08:40:24 -0700

    add about and welcome component and routing route

--- club-manager/src/app/app.module.ts
+++ club-manager/src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
+import { routing } from './app.routing';
+
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    AboutComponent
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
