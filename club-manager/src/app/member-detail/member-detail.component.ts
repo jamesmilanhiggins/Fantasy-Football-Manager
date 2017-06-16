c25e7de2c807cff424e1859771a94382338fcdbb
James Higgins <james.milan.higgins@gmail.com>
2017-06-16 09:25:38 -0700

    Add member-detail to display firebase data

--- club-manager/src/app/member-detail/member-detail.component.ts
+++ club-manager/src/app/member-detail/member-detail.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+import { Member } from '../member.model';
+import { MemberService } from '../member.service';
+import { FirebaseObjectObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-member-detail',
   templateUrl: './member-detail.component.html',
-  styleUrls: ['./member-detail.component.css']
+  styleUrls: ['./member-detail.component.css'],
+  providers: [MemberService]
 })
 export class MemberDetailComponent implements OnInit {
+  memberId: string;
+  memberToDisplay;
 
-  constructor() { }
-
+  constructor(
+  private route: ActivatedRoute,
+  private location: Location,
+  private memberService: MemberService) {}
   ngOnInit() {
+    this.route.params.forEach((urlParameters) => {
+      this.memberId = urlParameters['id'];
+   });
+   this.memberToDisplay = this.memberService.getMemberById(this.memberId);
   }
-
-}
+ }
