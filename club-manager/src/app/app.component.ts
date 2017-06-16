c2c2ec8cc704fc630b8804b5b5b80ea5843a7f78
James Higgins <james.milan.higgins@gmail.com>
2017-06-16 12:43:48 -0700

    add ngif statement to hide log in button if logged in, and hide logout button if logged out

--- club-manager/src/app/app.component.ts
+++ club-manager/src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [AngularFireAuth]
 })
 export class AppComponent {
   title = 'app';
+  user: Observable<firebase.User>;
+
+  
+  constructor(public afAuth: AngularFireAuth) {
+    this.user = afAuth.authState;
+  }
+
+  login(email, password) {
+    // this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+    this.afAuth.auth.signInWithEmailAndPassword(email, password).catch(function(error) {
+      // var errorCode = error.code;
+      // var errorMessage = error.message;
+      // console.log(errorMessage);
+    });
+  }
+
+  logout() {
+    this.afAuth.auth.signOut();
+  }
+
+  ngOnInit() {
+  }
+
 }
